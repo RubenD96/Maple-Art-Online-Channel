1bf6eccac9007e1645b890e6396d6360ac4e825c
Spoon <pengo@spoon.pepe>
2020-08-11 02:20:46 +0200

    These somehow didnt get added...

---
+++ src/main/java/client/shop/NPCShopItem.java
@@ -0,0 +1,17 @@
+package client.shop;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class NPCShopItem {
+
+    private final int id;
+    private int price, tokenId, tokenPrice, itemPeriod, levelLimited, stock;
+    private double unitPrice;
+    private short maxPerSlot, quantity;
+    private byte discountRate;
+}
