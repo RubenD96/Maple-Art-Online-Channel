9bd262d26342aceb132d5916a67c19eb6f70130f
Spoon <pengo@spoon.pepe>
2020-08-18 14:07:19 +0200

    MobMoveHandler 3rd party animation fix

--- src/main/java/net/maple/handlers/mob/MobMoveHandler.java
+++ src/main/java/net/maple/handlers/mob/MobMoveHandler.java
@@ -17,50 +17,55 @@ public class MobMoveHandler extends PacketHandler {
 
     @Override
     public void handlePacket(PacketReader reader, Client c) {
-        int id = reader.readInteger();
+        int oid = reader.readInteger();
 
         Field field = c.getCharacter().getField();
 
         if (field != null) { // might happen anytime user leaves the field but client still sent the packet when field was already set to null server sided
-            FieldControlledObject obj = field.getControlledObject(c.getCharacter(), id);
+            FieldControlledObject obj = field.getControlledObject(c.getCharacter(), oid);
             if (obj == null) {
                 return;
             }
             if (obj instanceof FieldNPC) {
-                System.err.println("[MobMoveHandler] Packet sent from FieldNPC object: " + id);
+                System.err.println("[MobMoveHandler] Packet sent from FieldNPC object: " + oid);
                 return;
             }
 
             FieldMob mob = (FieldMob) obj;
-            short mobCtrlSN = reader.readShort();
-            boolean mobMove = (reader.readByte() & 0xF) != 0;
-            byte split = reader.readByte();
-            int velocity = reader.readInteger();
-            reader.readByte();
+            short mobCtrlSN = reader.readShort(); // moveid
+            byte v7 = reader.readByte();
+            boolean oldSplit = (v7 & 0xF0) != 0;
+            boolean mobMoveStartResult = v7 > 0; // use skill //(v7 & 0xF) != 0;
+            byte curSplit = reader.readByte(); // the skill
+            int illegalVelocity = reader.readInteger();
+            byte v8 = reader.readByte();
+
+            boolean cheatedRandom = (v8 & 0xF0) != 0;
+            boolean cheatedCtrlMove = (v8 & 0xF) != 0;
 
             int multiTargetForBall = reader.readInteger(); // wtf
             IntStream.range(0, multiTargetForBall).forEach(i -> reader.readLong());
 
-            int areaAttack = reader.readInteger();
-            IntStream.range(0, areaAttack).forEach(i -> reader.readInteger());
+            int randTimeForAreaAttack = reader.readInteger();
+            IntStream.range(0, randTimeForAreaAttack).forEach(i -> reader.readInteger());
 
-            reader.readInteger();
-            reader.readInteger();
-            reader.readInteger();
-            reader.readInteger();
+            int unk1 = reader.readInteger(); // HackedCode
+            int unk2 = reader.readInteger();
+            int unk3 = reader.readInteger(); // HackedCodeCrc
+            int unk4 = reader.readInteger();
 
-            mob.getController().write(sendMobControl(mob, mobCtrlSN, mobMove));
-            mob.getField().broadcast(sendMobMovement(reader, mob, mobMove, split, velocity), mob.getController());
+            mob.getController().write(sendMobControl(mob, mobCtrlSN, mobMoveStartResult));
+            mob.getField().broadcast(sendMobMovement(reader, mob, mobMoveStartResult, curSplit, illegalVelocity), mob.getController());
         }
     }
 
-    private static Packet sendMobControl(FieldMob mob, short SN, boolean move) {
+    private static Packet sendMobControl(FieldMob mob, short mobCtrlSN, boolean mobMoveStartResult) {
         PacketWriter pw = new PacketWriter(13);
 
         pw.writeHeader(SendOpcode.MOB_CTRL_ACK);
         pw.writeInt(mob.getId());
-        pw.writeShort(SN);
-        pw.writeBool(move);
+        pw.writeShort(mobCtrlSN);
+        pw.writeBool(mobMoveStartResult);
         pw.writeShort(0); // nMP
         pw.write(0); // skill command
         pw.write(0); // skill level
@@ -68,18 +73,19 @@ public class MobMoveHandler extends PacketHandler {
         return pw.createPacket();
     }
 
-    private static Packet sendMobMovement(PacketReader r, FieldMob mob, boolean move, byte split, int velocity) {
+    private static Packet sendMobMovement(PacketReader r, FieldMob mob, boolean mobMoveStartResult, byte curSplit, int illegalVelocity) {
         PacketWriter pw = new PacketWriter(32);
 
         pw.writeHeader(SendOpcode.MOB_MOVE);
         pw.writeInt(mob.getId());
-        pw.writeBool(move);
-        pw.write(split);
-        pw.write(0);
-        pw.write(0);
-        pw.writeInt(velocity);
-        pw.writeInt(0);
-        pw.writeInt(0);
+        pw.writeBool(false); // NotForceLandWhenDiscord
+        pw.writeBool(mobMoveStartResult); // NotChangeAction
+        pw.write(0); // NextAttackPossible
+        pw.write(curSplit); // Left
+        pw.writeInt(illegalVelocity); // dwTargetInfo
+
+        pw.writeInt(0); // MultiTargetForBall (counter) if > 0, + int int
+        pw.writeInt(0); // RandTimeForAreaAttack (counter) if > 0, + int
 
         mob.move(r).encode(pw);
 
