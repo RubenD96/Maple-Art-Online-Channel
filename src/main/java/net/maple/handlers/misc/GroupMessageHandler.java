ce9d737c4b9e113a5480430944bcaefc48e5b816
Spoon <pengo@spoon.pepe>
2020-08-21 17:31:36 +0200

    Guild chat

--- src/main/java/net/maple/handlers/misc/GroupMessageHandler.java
+++ src/main/java/net/maple/handlers/misc/GroupMessageHandler.java
@@ -2,8 +2,6 @@ package net.maple.handlers.misc;
 
 import client.Character;
 import client.Client;
-import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import net.maple.SendOpcode;
 import net.maple.handlers.PacketHandler;
@@ -14,45 +12,52 @@ import util.packet.PacketWriter;
 public class GroupMessageHandler extends PacketHandler {
 
     @RequiredArgsConstructor
-    private enum ChatGroupType {
-        FRIEND(0x00),
-        PARTY(0x01),
-        GUILD(0x02),
-        ALLIANCE(0x03),
-        COUPLE(0x04),
-        TO_COUPLE(0x05),
-        EXPEDITION(0x06);
-
-        @Getter @NonNull private final int value;
+    private static final class ChatGroupType {
+        public static final byte FRIEND = 0x00;
+        public static final byte PARTY = 0x01;
+        public static final byte GUILD = 0x02;
+        public static final byte ALLIANCE = 0x03;
+        public static final byte COUPLE = 0x04;
+        public static final byte TO_COUPLE = 0x05;
+        public static final byte EXPEDITION = 0x06;
     }
 
     @Override
     public void handlePacket(PacketReader reader, Client c) {
         Character chr = c.getCharacter();
         reader.readInteger(); // timestamp
-        int type = reader.readByte(); // chat type
+        byte type = reader.readByte(); // chat type
         int size = reader.readByte(); // count members
         for (int i = 0; i < size; i++) {
             reader.readInteger(); // cid
         }
         String message = reader.readMapleString(); // the message
 
-        if (type == ChatGroupType.FRIEND.value) {
-            chr.getFriendList().sendMessage(multiChat(ChatGroupType.FRIEND, chr.getName(), message));
-        } else if (type == ChatGroupType.PARTY.value) {
-            if (chr.getParty() != null) {
-                chr.getParty().sendMessage(multiChat(ChatGroupType.PARTY, chr.getName(), message), chr.getId());
-            }
-        } else {
-            System.out.println("Unknown GroupMessageHandler type (" + type + ") from " + chr.getName());
+        switch (type) {
+            case ChatGroupType.FRIEND:
+                chr.getFriendList().sendMessage(multiChat(ChatGroupType.FRIEND, chr.getName(), message));
+                break;
+            case ChatGroupType.PARTY:
+                if (chr.getParty() != null) {
+                    chr.getParty().sendMessage(multiChat(ChatGroupType.PARTY, chr.getName(), message), chr.getId());
+                }
+            case ChatGroupType.GUILD:
+                if (chr.getGuild() != null) {
+                    chr.getGuild().broadcast(multiChat(ChatGroupType.GUILD, chr.getName(), message), chr);
+                }
+                break;
+            default:
+                System.out.println("Unknown GroupMessageHandler type (" + type + ") from " + chr.getName());
+                break;
+
         }
     }
 
-    public static Packet multiChat(ChatGroupType type, String name, String message) {
+    public static Packet multiChat(byte type, String name, String message) {
         PacketWriter pw = new PacketWriter(8);
 
         pw.writeHeader(SendOpcode.GROUP_MESSAGE);
-        pw.write(type.value);
+        pw.write(type);
         pw.writeMapleString(name);
         pw.writeMapleString(message);
 
