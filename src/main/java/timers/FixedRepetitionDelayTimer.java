26963a871b5b4071fcd5b036e7ff7e7b1ea2cfe0
Spoon <pengo@spoon.pepe>
2020-08-22 20:13:59 +0200

    FixedRepetitionDelayTimer

---
+++ src/main/java/timers/FixedRepetitionDelayTimer.java
@@ -0,0 +1,34 @@
+package timers;
+
+/**
+ * Immediately executes a Runnable, repeats it n amount of times with a delay of x-time milliseconds.
+ */
+public class FixedRepetitionDelayTimer extends BaseTimer {
+
+    private int n;
+
+    public FixedRepetitionDelayTimer(int time, Runnable runnable, int n) {
+        super(time, runnable);
+        if (n < 2) {
+            this.n = 0; // run() will be called, but it won't execute the Runnable
+            System.err.println("[FixedRepetitionDelayTimer] N("+n+") should be 2 or higher, use DelayTimer instead.");
+            return;
+        }
+        this.n = n;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (n != 0) {
+                runnable.run();
+                Thread.sleep(time);
+            }
+            n--;
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        } finally {
+            run();
+        }
+    }
+}
