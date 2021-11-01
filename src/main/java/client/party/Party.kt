cd18ce67fb7d8d4f63a8e80951eaf0621d124c0f
RubenD96 <RDoesburg@carellurvink.nl>
2021-11-01 13:37:16 +0100

    bossguard npc

--- src/main/java/client/party/Party.kt
+++ src/main/java/client/party/Party.kt
@@ -1,6 +1,7 @@
 package client.party
 
 import client.Character
+import field.Field
 import net.maple.packets.PartyPackets
 import net.server.Server.getCharacter
 import util.logging.LogType
@@ -8,7 +9,6 @@ import util.logging.Logger.log
 import util.packet.Packet
 import util.packet.PacketWriter
 import java.util.*
-import kotlin.collections.ArrayList
 
 class Party(leader: Character) {
 
@@ -51,6 +51,10 @@ class Party(leader: Character) {
             }
         }
         return null
+    }
+
+    fun getMembersOnSameField(field: Field): List<PartyMember> {
+        return onlineMembers.filter { getCharacter(it.cid)?.field == field }
     }
 
     fun sendMessage(packet: Packet, from: Int) {
@@ -167,158 +171,3 @@ class Party(leader: Character) {
         //pw.writeInt(0) // tagPOINT m_ptFieldPortal; y
     }
 }
-/*
-package client.party;
-
-import client.Character;
-import net.maple.packets.PartyPackets;
-import util.packet.Packet;
-import util.packet.PacketWriter;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-public class Party {
-
-    private static int availableId = 1;
-    private final int id;
-    private int leaderId;
-    private final List<PartyMember> members = new ArrayList<>();
-
-    public void setLeaderId(int leaderId) {
-        this.leaderId = leaderId;
-    }
-
-    public static int getAvailableId() {
-        return availableId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public int getLeaderId() {
-        return leaderId;
-    }
-
-    public List<PartyMember> getMembers() {
-        return members;
-    }
-
-    public Party(Character leader) {
-        this.id = availableId++;
-        this.leaderId = leader.getId();
-        addMember(leader);
-        System.out.println("created party with partyid " + id);
-    }
-
-    public synchronized void addMember(Character member) {
-        members.add(new PartyMember(member));
-    }
-
-    public synchronized PartyMember getMember(int id) {
-        for (PartyMember member : members) {
-            if (member.getCid() == id) {
-                return member;
-            }
-        }
-        return null;
-    }
-
-    public void sendMessage(Packet packet, int from) {
-        for (PartyMember member : getMembers()) {
-            if (member.isOnline() && from != member.getCid()) {
-                member.getCharacter().write(packet.clone());
-            }
-        }
-    }
-
-    public PartyMember getLeader() {
-        return getMember(leaderId);
-    }
-
-    public synchronized void update() {
-        for (PartyMember member : getMembers()) {
-            if (member.isOnline()) {
-                member.getCharacter().write(PartyPackets.updateParty(this, member.getChannel()).clone());
-                member.getCharacter().updatePartyHP(true);
-            }
-        }
-    }
-
-    public synchronized PartyMember expel(int cid) {
-        PartyMember toExpel = null;
-        for (PartyMember member : members) {
-            if (member.getCid() == cid) {
-                toExpel = member;
-                break;
-            }
-        }
-        if (toExpel != null) {
-            members.remove(toExpel);
-        }
-        return toExpel;
-    }
-
-    public synchronized List<PartyMember> getOnlineMembers() {
-        List<PartyMember> members = new ArrayList<>();
-        for (PartyMember member : this.members) {
-            if (member.isOnline()) {
-                members.add(member);
-            }
-        }
-        return members;
-    }
-
-    public synchronized PartyMember getRandomOnline(int exclude) {
-        PartyMember member;
-        do {
-            member = members.get(new Random().nextInt(members.size()));
-        } while (member.getCid() == exclude);
-        return member;
-    }
-
-    public void encodePartyData(PacketWriter pw, int memChannel) {
-        List<PartyMember> members = new ArrayList<>(this.members);
-
-        while (members.size() < 6) {
-            members.add(new PartyMember());
-        }
-
-        encodeMembers(pw, members, leaderId); // PARTYMEMBER party
-        members.forEach(m -> {
-            if (m.getChannel() == memChannel) {
-                pw.writeInt(m.getField());
-            } else {
-                pw.writeInt(0);
-            }
-        }); // unsigned int adwFieldID[6]
-        members.forEach(m -> encodePortal(pw)); // PARTYDATA::TOWNPORTAL aTownPortal[6]
-        members.forEach(m -> pw.writeInt(0)); // int aPQReward
-        members.forEach(m -> pw.writeInt(0)); // int aPQRewardType
-        pw.writeInt(0); // unsigned int dwPQRewardMobTemplateID
-        pw.writeInt(0); // int bPQReward
-    }
-
-    public void encodeMembers(PacketWriter pw, List<PartyMember> members, int bossId) {
-        members.forEach(m -> pw.writeInt(m.getCid())); // unsigned int adwCharacterID[6]
-        members.forEach(m -> {
-            pw.writeFixedString(m.getName(), 13); // char asCharacterName[6][13]
-        });
-        members.forEach(m -> pw.writeInt(m.getJob())); // int anJob[6]
-        members.forEach(m -> pw.writeInt(m.getLevel())); // int anLevel[6]
-        members.forEach(m -> pw.writeInt(m.getChannel())); // int anChannelID[6]
-        pw.writeInt(bossId); // unsigned int dwPartyBossCharacterID
-    }
-
-    public void encodePortal(PacketWriter pw) {
-        pw.writeInt(999999999); // dwTownID
-        pw.writeInt(999999999); // dwFieldID
-        pw.writeInt(0); // nSkillID
-        pw.writeInt(0); // tagPOINT m_ptFieldPortal; x
-        pw.writeInt(0); // tagPOINT m_ptFieldPortal; y
-    }
-}
-
- */
---
+++ src/main/java/scripting/scripts/npc/BossGuard.kt
@@ -0,0 +1,150 @@
+package scripting.scripts.npc
+
+import client.Client
+import client.party.PartyMember
+import field.obj.life.FieldMob
+import net.server.Server
+import scripting.dialog.DialogContext
+import scripting.dialog.DialogUtils.bold
+import scripting.dialog.DialogUtils.red
+import scripting.dialog.npc.NPCScript
+import scripting.dialog.npc.Npc
+
+@Npc([1000]) // todo; list of id's of boss guards
+class BossGuard : NPCScript() {
+
+    private data class FloorInfo(
+        val preMessage: String, // first message before you enter
+        val boss: Int, // boss mob id
+        val exitMap: Int, // map to tp to after the boss is defeated
+        val fightMessage: String, // during fight, boss not defeated yet
+        val endMessage: String, // boss defeated
+        val warpMessage: String, // message after warping out
+        val nextTownMessage: String = "", // talking to the npc again on the new floor (not always present)
+        val prepareMessage: String = "Be sure to prepare properly!" // clicking end chat before starting
+        //val returnMap: Int = -1 // map to tp to if you quit the bossfight, defaulted to floor + 999 (1999 for floor 1)
+    )
+
+    private val DialogContext.chr get() = c.character
+    private val DialogContext.mapMembers get() = chr.party!!.getMembersOnSameField(chr.field)
+    private val DialogContext.info: FloorInfo
+        get() {
+            val sub = chr.fieldId % 1000
+            if (sub == 998 || sub == 999) {
+                return floors[chr.fieldId / 1000]!!
+            }
+            return floors[(chr.fieldId / 1000) - 1]!!
+        }
+
+    private companion object {
+        val floors: Map<Int, FloorInfo> = mapOf(
+            1 to FloorInfo(
+                "Sup before start",
+                100100,
+                2000,
+                "Hehe u gonna die",
+                "Wow u stronk!",
+                "Next town is just up ahead",
+                "Boyyyyyyyyyy u done did it"
+            )
+        )
+    }
+
+    override fun execute(c: Client) {
+        start(c) {
+            with(it) {
+                if (chr.fieldId % 1000 == 998) beforeFight()
+                else if (chr.fieldId % 1000 == 999) duringFight()
+                else afterFight()
+            }
+        }
+    }
+
+    private fun DialogContext.beforeFight() {
+        sendMessage(
+            info.preMessage,
+            next = {
+                chr.party?.let {
+                    if (it.getMembers().size == 1) return@sendMessage // single player party (no members)
+                    if (it.leaderId == chr.id) {
+                        var message = "Are you sure you want to fight the boss with your ${"party".red().bold()}?"
+                        if (mapMembers.size != it.onlineMembers.size) {
+                            message += "\r\n\r\nNot all party members are in the map, only the party members that are present on this map will participate."
+                                .bold().red()
+                        }
+                        sendMessage(
+                            message,
+                            yes = { warpStart(mapMembers) }
+                        )
+                    } else {
+                        sendMessage("You are not the party leader. If you want to try the boss solo please leave your current party first.")
+                    }
+                    return@sendMessage
+                }
+
+                sendMessage(
+                    "Are you sure you want to fight the boss ${"alone".red().bold()}?",
+                    yes = { warpStart() }
+                )
+            }
+        )
+    }
+
+    private fun DialogContext.warpStart(members: List<PartyMember>? = null) {
+        members?.let {
+            if (mayPartyEnter(it)) {
+                // pre-load the field once, so everybody goes to the same field, and not 6 different instances
+                val field = chr.getChannel().fieldManager.getField(chr.fieldId + 1) // 998 + 1 = 999 :)
+                it.forEach { member ->
+                    Server.getCharacter(member.cid)?.changeField(field)
+                }
+            } else {
+                sendMessage(
+                    "1 or more members in the party have already defeated this boss once while 1 or more members haven't." +
+                        "\r\nMake sure your party is either full of people that defeated the boss before or full of people that haven't.",
+                    ok = { clearStates() }, // no prepareMessage
+                    end = { clearStates() }
+                )
+            }
+        } ?: run { // solo
+            chr.changeField(chr.getChannel().fieldManager.getField(chr.fieldId + 1))
+        }
+    }
+
+    private fun DialogContext.mayPartyEnter(members: List<PartyMember>): Boolean {
+        var killedCounter = 0
+        members.forEach {
+            Server.getCharacter(it.cid)!!.mobKills[info.boss]?.let { killedCounter++ }
+        }
+
+        if (killedCounter > 0) return killedCounter == members.size
+        return true
+    }
+
+    private fun DialogContext.duringFight() {
+        chr.field.getObjects<FieldMob>().firstOrNull { it.template.id == info.boss }?.let {
+            sendMessage(info.fightMessage)
+        } ?: run {
+            sendMessage(
+                info.endMessage + "\r\n" +
+                        "Do you wish to go to the next floor right now?",
+                yes = {
+                    chr.changeField(info.exitMap)
+                    sendMessage(info.warpMessage)
+                },
+                no = {
+                    sendMessage("Let me know when you're ready!")
+                }
+            )
+        }
+    }
+
+    private fun DialogContext.afterFight() {
+        sendMessage(info.nextTownMessage)
+    }
+
+    override fun DialogContext.onEnd() {
+        if (chr.fieldId % 1000 == 998) endMessage(info.prepareMessage)
+        else clearStates()
+    }
+}
